bc5ab66cea8858202c02b234bf689a84d2c08b2d
Hirayamaru <r202302225kl@jindai.jp>
2025-07-31 15:54:15 +0900

    20250731

---
+++ doyolab/js/overlay_anim.js
@@ -0,0 +1,538 @@
+(function (cjs, an) {
+
+var p; // shortcut to reference prototypes
+var lib={};var ss={};var img={};
+lib.ssMetadata = [
+		{name:"overlay_anim_atlas_1", frames: [[0,0,163,218],[0,220,163,218]]}
+];
+
+
+(lib.AnMovieClip = function(){
+	this.actionFrames = [];
+	this.ignorePause = false;
+	this.gotoAndPlay = function(positionOrLabel){
+		cjs.MovieClip.prototype.gotoAndPlay.call(this,positionOrLabel);
+	}
+	this.play = function(){
+		cjs.MovieClip.prototype.play.call(this);
+	}
+	this.gotoAndStop = function(positionOrLabel){
+		cjs.MovieClip.prototype.gotoAndStop.call(this,positionOrLabel);
+	}
+	this.stop = function(){
+		cjs.MovieClip.prototype.stop.call(this);
+	}
+}).prototype = p = new cjs.MovieClip();
+// symbols:
+
+
+
+(lib.fxn20250325064146044 = function() {
+	this.initialize(img.fxn20250325064146044);
+}).prototype = p = new cjs.Bitmap();
+p.nominalBounds = new cjs.Rectangle(0,0,3024,4032);
+
+
+(lib.fxn20250325071015353 = function() {
+	this.initialize(img.fxn20250325071015353);
+}).prototype = p = new cjs.Bitmap();
+p.nominalBounds = new cjs.Rectangle(0,0,3025,4034);
+
+
+(lib.fxn20250406173524655 = function() {
+	this.initialize(img.fxn20250406173524655);
+}).prototype = p = new cjs.Bitmap();
+p.nominalBounds = new cjs.Rectangle(0,0,3026,4035);
+
+
+(lib.fxn20250415161513472 = function() {
+	this.initialize(img.fxn20250415161513472);
+}).prototype = p = new cjs.Bitmap();
+p.nominalBounds = new cjs.Rectangle(0,0,3025,4033);
+
+
+(lib.ビットマップ1 = function() {
+	this.initialize(ss["overlay_anim_atlas_1"]);
+	this.gotoAndStop(0);
+}).prototype = p = new cjs.Sprite();
+
+
+
+(lib.ビットマップ2 = function() {
+	this.initialize(ss["overlay_anim_atlas_1"]);
+	this.gotoAndStop(1);
+}).prototype = p = new cjs.Sprite();
+// helper functions:
+
+function mc_symbol_clone() {
+	var clone = this._cloneProps(new this.constructor(this.mode, this.startPosition, this.loop, this.reversed));
+	clone.gotoAndStop(this.currentFrame);
+	clone.paused = this.paused;
+	clone.framerate = this.framerate;
+	return clone;
+}
+
+function getMCSymbolPrototype(symbol, nominalBounds, frameBounds) {
+	var prototype = cjs.extend(symbol, cjs.MovieClip);
+	prototype.clone = mc_symbol_clone;
+	prototype.nominalBounds = nominalBounds;
+	prototype.frameBounds = frameBounds;
+	return prototype;
+	}
+
+
+(lib.ローダンセマム = function(mode,startPosition,loop,reversed) {
+if (loop == null) { loop = true; }
+if (reversed == null) { reversed = false; }
+	var props = new Object();
+	props.mode = mode;
+	props.startPosition = startPosition;
+	props.labels = {};
+	props.loop = loop;
+	props.reversed = reversed;
+	cjs.MovieClip.apply(this,[props]);
+
+	// レイヤー_1
+	this.instance = new lib.fxn20250415161513472();
+	this.instance.setTransform(0,-4,0.054,0.054);
+
+	this.timeline.addTween(cjs.Tween.get(this.instance).wait(1));
+
+	this._renderFirstFrame();
+
+}).prototype = p = new cjs.MovieClip();
+p.nominalBounds = new cjs.Rectangle(0,-4,163.4,217.7);
+
+
+(lib.ユキヤナギ = function(mode,startPosition,loop,reversed) {
+if (loop == null) { loop = true; }
+if (reversed == null) { reversed = false; }
+	var props = new Object();
+	props.mode = mode;
+	props.startPosition = startPosition;
+	props.labels = {};
+	props.loop = loop;
+	props.reversed = reversed;
+	cjs.MovieClip.apply(this,[props]);
+
+	// レイヤー_1
+	this.instance = new lib.fxn20250325064146044();
+	this.instance.setTransform(0,0,0.054,0.054);
+
+	this.timeline.addTween(cjs.Tween.get(this.instance).wait(1));
+
+	this._renderFirstFrame();
+
+}).prototype = p = new cjs.MovieClip();
+p.nominalBounds = new cjs.Rectangle(0,0,163.4,217.8);
+
+
+(lib.シンボル1 = function(mode,startPosition,loop,reversed) {
+if (loop == null) { loop = true; }
+if (reversed == null) { reversed = false; }
+	var props = new Object();
+	props.mode = mode;
+	props.startPosition = startPosition;
+	props.labels = {};
+	props.loop = loop;
+	props.reversed = reversed;
+	cjs.MovieClip.apply(this,[props]);
+
+	// レイヤー_1
+	this.instance = new lib.fxn20250406173524655();
+	this.instance.setTransform(0,0,0.054,0.054);
+
+	this.timeline.addTween(cjs.Tween.get(this.instance).wait(1));
+
+	this._renderFirstFrame();
+
+}).prototype = getMCSymbolPrototype(lib.シンボル1, new cjs.Rectangle(0,0,163.4,217.9), null);
+
+
+(lib.クローバー = function(mode,startPosition,loop,reversed) {
+if (loop == null) { loop = true; }
+if (reversed == null) { reversed = false; }
+	var props = new Object();
+	props.mode = mode;
+	props.startPosition = startPosition;
+	props.labels = {};
+	props.loop = loop;
+	props.reversed = reversed;
+	cjs.MovieClip.apply(this,[props]);
+
+	// レイヤー_1
+	this.instance = new lib.fxn20250325071015353();
+	this.instance.setTransform(0,0,0.054,0.054);
+
+	this.timeline.addTween(cjs.Tween.get(this.instance).wait(1));
+
+	this._renderFirstFrame();
+
+}).prototype = p = new cjs.MovieClip();
+p.nominalBounds = new cjs.Rectangle(0,0,163.4,217.8);
+
+
+(lib.オーバーレイ４ = function(mode,startPosition,loop,reversed) {
+if (loop == null) { loop = true; }
+if (reversed == null) { reversed = false; }
+	var props = new Object();
+	props.mode = mode;
+	props.startPosition = startPosition;
+	props.labels = {};
+	props.loop = loop;
+	props.reversed = reversed;
+	cjs.MovieClip.apply(this,[props]);
+
+	// レイヤー_1
+	this.shape = new cjs.Shape();
+	this.shape.graphics.f("#345745").s().p("AswRBMAAAgiBIZhAAMAAAAiBg");
+	this.shape.setTransform(-75.475,-114.025);
+
+	this.timeline.addTween(cjs.Tween.get(this.shape).wait(1));
+
+	this._renderFirstFrame();
+
+}).prototype = p = new cjs.MovieClip();
+p.nominalBounds = new cjs.Rectangle(-157.1,-222.9,163.29999999999998,217.8);
+
+
+(lib.オーバーレイ３ = function(mode,startPosition,loop,reversed) {
+if (loop == null) { loop = true; }
+if (reversed == null) { reversed = false; }
+	var props = new Object();
+	props.mode = mode;
+	props.startPosition = startPosition;
+	props.labels = {};
+	props.loop = loop;
+	props.reversed = reversed;
+	cjs.MovieClip.apply(this,[props]);
+
+	// レイヤー_1
+	this.shape = new cjs.Shape();
+	this.shape.graphics.f("#A1B293").s().p("AswRBMAAAgiBIZhAAMAAAAiBg");
+	this.shape.setTransform(81.675,60.825);
+
+	this.timeline.addTween(cjs.Tween.get(this.shape).wait(1));
+
+	this._renderFirstFrame();
+
+}).prototype = p = new cjs.MovieClip();
+p.nominalBounds = new cjs.Rectangle(0,-48,163.4,217.7);
+
+
+(lib.オーバーレイ２ = function(mode,startPosition,loop,reversed) {
+if (loop == null) { loop = true; }
+if (reversed == null) { reversed = false; }
+	var props = new Object();
+	props.mode = mode;
+	props.startPosition = startPosition;
+	props.labels = {};
+	props.loop = loop;
+	props.reversed = reversed;
+	cjs.MovieClip.apply(this,[props]);
+
+	// レイヤー_1
+	this.instance = new lib.ビットマップ2();
+
+	this.timeline.addTween(cjs.Tween.get(this.instance).wait(1));
+
+	this._renderFirstFrame();
+
+}).prototype = p = new cjs.MovieClip();
+p.nominalBounds = new cjs.Rectangle(0,0,163,218);
+
+
+(lib.オーバーレイ１ = function(mode,startPosition,loop,reversed) {
+if (loop == null) { loop = true; }
+if (reversed == null) { reversed = false; }
+	var props = new Object();
+	props.mode = mode;
+	props.startPosition = startPosition;
+	props.labels = {};
+	props.loop = loop;
+	props.reversed = reversed;
+	cjs.MovieClip.apply(this,[props]);
+
+	// レイヤー_1
+	this.instance = new lib.ビットマップ1();
+	this.instance.setTransform(-6,172);
+
+	this.timeline.addTween(cjs.Tween.get(this.instance).wait(1));
+
+	this._renderFirstFrame();
+
+}).prototype = p = new cjs.MovieClip();
+p.nominalBounds = new cjs.Rectangle(-6,172,163,218);
+
+
+(lib.ツバキ = function(mode,startPosition,loop,reversed) {
+if (loop == null) { loop = true; }
+if (reversed == null) { reversed = false; }
+	var props = new Object();
+	props.mode = mode;
+	props.startPosition = startPosition;
+	props.labels = {};
+	props.loop = loop;
+	props.reversed = reversed;
+	cjs.MovieClip.apply(this,[props]);
+
+	// レイヤー_1
+	this.instance = new lib.シンボル1();
+	this.instance.setTransform(81.7,108.9,1,1,0,0,0,81.7,108.9);
+
+	this.timeline.addTween(cjs.Tween.get(this.instance).wait(20));
+
+	this._renderFirstFrame();
+
+}).prototype = p = new cjs.MovieClip();
+p.nominalBounds = new cjs.Rectangle(0,0,163.4,217.9);
+
+
+// stage content:
+(lib.HP_check = function(mode,startPosition,loop,reversed) {
+if (loop == null) { loop = true; }
+if (reversed == null) { reversed = false; }
+	var props = new Object();
+	props.mode = mode;
+	props.startPosition = startPosition;
+	props.labels = {};
+	props.loop = loop;
+	props.reversed = reversed;
+	cjs.MovieClip.apply(this,[props]);
+
+	this.actionFrames = [0];
+	this.isSingleFrame = false;
+	// timeline functions:
+	this.frame_0 = function() {
+		if(this.isSingleFrame) {
+			return;
+		}
+		if(this.totalFrames == 1) {
+			this.isSingleFrame = true;
+		}
+		const root = this; 
+		
+		/* -- ① マウスオーバーを機能させる宣言 -------------------- */
+		stage.enableMouseOver();
+		
+		/* -- ② 初期状態でオーバーレイを非表示 -------------------- */
+		root.overlay1_mc.alpha = 0;
+		root.overlay2_mc.alpha = 0;
+		root.overlay3_mc.alpha = 0;
+		root.overlay4_mc.alpha = 0;
+		
+		/* -- ③ オーバーレイはマウスイベントを拾わないようにする -- */
+		root.overlay1_mc.mouseEnabled =
+		root.overlay2_mc.mouseEnabled =
+		root.overlay3_mc.mouseEnabled =
+		root.overlay4_mc.mouseEnabled = false;
+		
+		/* -- ④ ホバー設定を各ペアに適用 --------------------------- */
+		setupHover(root.tubaki,      root.overlay1_mc);
+		setupHover(root.yukiyanagi,  root.overlay2_mc);
+		setupHover(root.clover,      root.overlay3_mc);
+		setupHover(root.Rhodanthemum,root.overlay4_mc);
+		
+		/* -- ⑤ ホバーイベントを登録する関数 ----------------------- */
+		function setupHover(image, overlay) {
+		    image.cursor = "pointer";
+		
+		    image.on("mouseover", () => {
+		        createjs.Tween.get(overlay).to({ alpha: 0.5}, 300);
+		    });
+		
+		    image.on("mouseout", () => {
+		        createjs.Tween.get(overlay).to({ alpha: 0 }, 300);
+		    });
+		}
+	}
+
+	// actions tween:
+	this.timeline.addTween(cjs.Tween.get(this).call(this.frame_0).wait(1));
+
+	// ローダンセマム
+	this.overlay4_mc = new lib.オーバーレイ４();
+	this.overlay4_mc.name = "overlay4_mc";
+	this.overlay4_mc.setTransform(1044,480.45,1.3777,1.3777,0,0,0,81.7,108.9);
+	this.overlay4_mc.alpha = 0;
+
+	this.Rhodanthemum = new lib.ローダンセマム();
+	this.Rhodanthemum.name = "Rhodanthemum";
+	this.Rhodanthemum.setTransform(827.5,173.35,1.378,1.378,0,0,0,81.7,104.9);
+
+	this.timeline.addTween(cjs.Tween.get({}).to({state:[{t:this.Rhodanthemum},{t:this.overlay4_mc}]}).wait(1));
+
+	// クローバー
+	this.overlay3_mc = new lib.オーバーレイ３();
+	this.overlay3_mc.name = "overlay3_mc";
+	this.overlay3_mc.setTransform(589.55,241.2,1.3777,1.3777,0,0,0,81.7,108.9);
+	this.overlay3_mc.alpha = 0;
+
+	this.clover = new lib.クローバー();
+	this.clover.name = "clover";
+	this.clover.setTransform(589.6,175.05,1.3774,1.3774,0,0,0,81.7,109);
+
+	this.timeline.addTween(cjs.Tween.get({}).to({state:[{t:this.clover},{t:this.overlay3_mc}]}).wait(1));
+
+	// ユキヤナギ
+	this.overlay2_mc = new lib.オーバーレイ２();
+	this.overlay2_mc.name = "overlay2_mc";
+	this.overlay2_mc.setTransform(351.95,175.3,1.3804,1.3807,0,0,0,81.7,108.9);
+	this.overlay2_mc.alpha = 0;
+
+	this.yukiyanagi = new lib.ユキヤナギ();
+	this.yukiyanagi.name = "yukiyanagi";
+	this.yukiyanagi.setTransform(351.7,174.9,1.3777,1.3777,0,0,0,81.7,108.9);
+
+	this.timeline.addTween(cjs.Tween.get({}).to({state:[{t:this.yukiyanagi},{t:this.overlay2_mc}]}).wait(1));
+
+	// ツバキ
+	this.overlay1_mc = new lib.オーバーレイ１();
+	this.overlay1_mc.name = "overlay1_mc";
+	this.overlay1_mc.setTransform(189.3,154.95,1.3804,1.3807,0,0,0,129.1,265.6);
+	this.overlay1_mc.alpha = 0;
+
+	this.tubaki = new lib.ツバキ();
+	this.tubaki.name = "tubaki";
+	this.tubaki.setTransform(114.85,175.05,1.3768,1.3771,0,0,0,81.8,109);
+
+	this.timeline.addTween(cjs.Tween.get({}).to({state:[{t:this.tubaki},{t:this.overlay1_mc}]}).wait(1));
+
+	this._renderFirstFrame();
+
+}).prototype = p = new lib.AnMovieClip();
+p.nominalBounds = new cjs.Rectangle(472.3,198.3,467.7,128.5);
+// library properties:
+lib.properties = {
+	id: 'E226CF9421B64910B01BF579E3526604',
+	width: 940,
+	height: 350,
+	fps: 30,
+	color: "#FFFFFF",
+	opacity: 1.00,
+	manifest: [
+		{src:"images/fxn20250325064146044.jpeg", id:"fxn20250325064146044"},
+		{src:"images/fxn20250325071015353.jpeg", id:"fxn20250325071015353"},
+		{src:"images/fxn20250406173524655.jpeg", id:"fxn20250406173524655"},
+		{src:"images/fxn20250415161513472.jpeg", id:"fxn20250415161513472"},
+		{src:"images/overlay_anim_atlas_1.png", id:"overlay_anim_atlas_1"}
+	],
+	preloads: []
+};
+
+
+
+// bootstrap callback support:
+
+(lib.Stage = function(canvas) {
+	createjs.Stage.call(this, canvas);
+}).prototype = p = new createjs.Stage();
+
+p.setAutoPlay = function(autoPlay) {
+	this.tickEnabled = autoPlay;
+}
+p.play = function() { this.tickEnabled = true; this.getChildAt(0).gotoAndPlay(this.getTimelinePosition()) }
+p.stop = function(ms) { if(ms) this.seek(ms); this.tickEnabled = false; }
+p.seek = function(ms) { this.tickEnabled = true; this.getChildAt(0).gotoAndStop(lib.properties.fps * ms / 1000); }
+p.getDuration = function() { return this.getChildAt(0).totalFrames / lib.properties.fps * 1000; }
+
+p.getTimelinePosition = function() { return this.getChildAt(0).currentFrame / lib.properties.fps * 1000; }
+
+an.bootcompsLoaded = an.bootcompsLoaded || [];
+if(!an.bootstrapListeners) {
+	an.bootstrapListeners=[];
+}
+
+an.bootstrapCallback=function(fnCallback) {
+	an.bootstrapListeners.push(fnCallback);
+	if(an.bootcompsLoaded.length > 0) {
+		for(var i=0; i<an.bootcompsLoaded.length; ++i) {
+			fnCallback(an.bootcompsLoaded[i]);
+		}
+	}
+};
+
+an.compositions = an.compositions || {};
+an.compositions['E226CF9421B64910B01BF579E3526604'] = {
+	getStage: function() { return exportRoot.stage; },
+	getLibrary: function() { return lib; },
+	getSpriteSheet: function() { return ss; },
+	getImages: function() { return img; }
+};
+
+an.compositionLoaded = function(id) {
+	an.bootcompsLoaded.push(id);
+	for(var j=0; j<an.bootstrapListeners.length; j++) {
+		an.bootstrapListeners[j](id);
+	}
+}
+
+an.getComposition = function(id) {
+	return an.compositions[id];
+}
+
+
+an.makeResponsive = function(isResp, respDim, isScale, scaleType, domContainers) {		
+	var lastW, lastH, lastS=1;		
+	window.addEventListener('resize', resizeCanvas);		
+	resizeCanvas();		
+	function resizeCanvas() {			
+		var w = lib.properties.width, h = lib.properties.height;			
+		var iw = window.innerWidth, ih=window.innerHeight;			
+		var pRatio = window.devicePixelRatio || 1, xRatio=iw/w, yRatio=ih/h, sRatio=1;			
+		if(isResp) {                
+			if((respDim=='width'&&lastW==iw) || (respDim=='height'&&lastH==ih)) {                    
+				sRatio = lastS;                
+			}				
+			else if(!isScale) {					
+				if(iw<w || ih<h)						
+					sRatio = Math.min(xRatio, yRatio);				
+			}				
+			else if(scaleType==1) {					
+				sRatio = Math.min(xRatio, yRatio);				
+			}				
+			else if(scaleType==2) {					
+				sRatio = Math.max(xRatio, yRatio);				
+			}			
+		}
+		domContainers[0].width = w * pRatio * sRatio;			
+		domContainers[0].height = h * pRatio * sRatio;
+		domContainers.forEach(function(container) {				
+			container.style.width = w * sRatio + 'px';				
+			container.style.height = h * sRatio + 'px';			
+		});
+		stage.scaleX = pRatio*sRatio;			
+		stage.scaleY = pRatio*sRatio;
+		lastW = iw; lastH = ih; lastS = sRatio;            
+		stage.tickOnUpdate = false;            
+		stage.update();            
+		stage.tickOnUpdate = true;		
+	}
+}
+an.handleSoundStreamOnTick = function(event) {
+	if(!event.paused){
+		var stageChild = stage.getChildAt(0);
+		if(!stageChild.paused || stageChild.ignorePause){
+			stageChild.syncStreamSounds();
+		}
+	}
+}
+an.handleFilterCache = function(event) {
+	if(!event.paused){
+		var target = event.target;
+		if(target){
+			if(target.filterCacheList){
+				for(var index = 0; index < target.filterCacheList.length ; index++){
+					var cacheInst = target.filterCacheList[index];
+					if((cacheInst.startFrame <= target.currentFrame) && (target.currentFrame <= cacheInst.endFrame)){
+						cacheInst.instance.cache(cacheInst.x, cacheInst.y, cacheInst.w, cacheInst.h);
+					}
+				}
+			}
+		}
+	}
+}
+
+
+})(createjs = createjs||{}, AdobeAn = AdobeAn||{});
+var createjs, AdobeAn;
