923b3eab2a12ca160d02065e4db09ab58990734c
Hirayamaru <r202302225kl@jindai.jp>
2025-11-10 22:51:10 +0900

    HPを更新

--- doyolab/js/script.js
+++ doyolab/js/script.js
@@ -1,1 +1,14 @@
 $(function(){$("nav div.panel").hide();$(".menu").click(function(){$(this).toggleClass("menuOpen").next().slideToggle();});})
+// ===== スクロールでフェードインアニメーション =====
+document.addEventListener("scroll", function() {
+    const elements = document.querySelectorAll(".fadein");
+    const windowHeight = window.innerHeight;
+  
+    elements.forEach(el => {
+      const rect = el.getBoundingClientRect();
+      if (rect.top < windowHeight - 100) {
+        el.classList.add("visible");
+      }
+    });
+  });
+  
