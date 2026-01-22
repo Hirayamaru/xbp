b5a9c39013cc65497aa53f0705c47864c787953f
Hirayamaru <r202302225kl@jindai.jp>
2026-01-22 21:35:27 +0900

    a

--- doyolab/js/script.js
+++ doyolab/js/script.js
@@ -1,14 +1,20 @@
-$(function(){$("nav div.panel").hide();$(".menu").click(function(){$(this).toggleClass("menuOpen").next().slideToggle();});})
-// ===== スクロールでフェードインアニメーション =====
-document.addEventListener("scroll", function() {
-    const elements = document.querySelectorAll(".fadein");
-    const windowHeight = window.innerHeight;
-  
-    elements.forEach(el => {
-      const rect = el.getBoundingClientRect();
-      if (rect.top < windowHeight - 100) {
-        el.classList.add("visible");
-      }
-    });
+// メニューの開閉
+$(function(){
+  $("nav div.panel").hide();
+  $(".menu").click(function(){
+    $(this).toggleClass("menuOpen").next().slideToggle();
   });
-  
+});
+
+// スクロールでフェードイン
+document.addEventListener("scroll", function() {
+  const elements = document.querySelectorAll(".fadein");
+  const windowHeight = window.innerHeight;
+
+  elements.forEach(el => {
+    const rect = el.getBoundingClientRect();
+    if (rect.top < windowHeight - 100) {
+      el.classList.add("visible");
+    }
+  });
+});
